63160a6c437ba1116dea7eaf0ad022a7dabc2c9f
Nailah <dulieprenailah@gmail.com>
2020-07-13 20:02:14 -0400

    Testing file push

---
+++ Assets/Scripts/Restart.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class LoadScenesA : MonoBehaviour
+//public class Restart : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("LoadSceneA");
+    }
+
+    public void LoadA (string scenename)
+    {
+        Debug.Log("SampleScene" + scenename);
+        SceneManager.LoadScene(scenename);
+    }
+}
